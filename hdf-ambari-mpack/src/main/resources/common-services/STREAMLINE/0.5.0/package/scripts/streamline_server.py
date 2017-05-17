bbebd22fd7dbe886ec02f3de7f61d7ed27bd958c
Sriharsha Chintalapani <harsha@hortonworks.com>
2017-05-17 10:11:08 -0700

    BUG-80999: SAM adminPrincipals config syntax is wrong.

--- hdf-ambari-mpack/src/main/resources/common-services/STREAMLINE/0.5.0/package/scripts/streamline_server.py
+++ hdf-ambari-mpack/src/main/resources/common-services/STREAMLINE/0.5.0/package/scripts/streamline_server.py
@@ -89,7 +89,7 @@ class StreamlineServer(Script):
           kinit_cmd = format("{kinit_path_local} -kt {params.streamline_keytab_path} {params.streamline_jaas_principal};")
           return_code, out = shell.checked_call(kinit_cmd,
                                                 path='/usr/sbin:/sbin:/usr/local/bin:/bin:/usr/bin',
-                                                user="root")
+                                                user=params.streamline_user)
         wait_until_server_starts()
         Execute(params.bootstrap_run_cmd,
                 user="root")
