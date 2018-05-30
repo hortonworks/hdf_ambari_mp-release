287f3f2192ddc30265e9c89d7043d12cf6c89e30
Eugene Chekanskiy <echekanskiy@hortonworks.com>
2018-05-31 01:04:05 +0300

    RMP-11290 Update HDF Management Pack to support include Amazon Linux … (#222)
    * RMP-11290 Update HDF Management Pack to support include Amazon Linux repository.
    update_hdp.sh script now accepts username:password string as third argument.
    
    * RMP-11290 - fix package install issue.

--- update_hdp.sh
+++ update_hdp.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 repo_proto=${1:-https}
 repo_branch=${2:-AMBARI-2.7.0.0}
-repo_url=$(([ "$repo_proto" == "ssh" ] && echo "git@github.com:hortonworks/hdp_ambari_definitions.git") || ([ "$repo_proto" == "https" ] && echo "https://github.com/hortonworks/hdp_ambari_definitions.git") || exit -1)
+username_password=$3
+username_password=$( [ "$username_password" ] && echo "${username_password}@")
+repo_url=$(([ "$repo_proto" == "ssh" ] && echo "git@github.com:hortonworks/hdp_ambari_definitions.git") || ([ "$repo_proto" == "https" ] && echo "https://${username_password}github.com/hortonworks/hdp_ambari_definitions.git") || exit -1)
 [ $? -ne 0 ] && (echo "Unknown protocol $repo_proto, must be ssh or https"; exit -1)
 rm -rf hdp_ambari_definitions
 rm -rf .git/modules/hdp_ambari_definitions
