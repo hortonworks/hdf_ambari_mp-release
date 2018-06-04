7054af155ac5cf6117bf6ac1152f8dd86fbea76a
Yolanda Davis <yolanda.m.davis@gmail.com>
2018-06-04 10:14:14 -0400

    BUG-104436 - updated run_ca wrapper script to use parent pid due to changes in tls-toolkit to use exec (#225)

--- hdf-ambari-mpack/src/main/resources/common-services/NIFI/1.0.0/package/files/run_ca.sh
+++ hdf-ambari-mpack/src/main/resources/common-services/NIFI/1.0.0/package/files/run_ca.sh
@@ -39,24 +39,9 @@ read -r -d '' WAIT_FOR_LOG << 'EOF'
   done
 EOF
 
-read -r -d '' WAIT_FOR_CHILD << 'EOF'
-  SHELL_PID="$0"
-  PID_FILE="$1"
-  CHILD_PROCESS="$(pgrep -P "$SHELL_PID" java)"
-  while [ -z "$CHILD_PROCESS" ]; do
-    echo "Waiting for child java process to exist"
-    sleep 1;
-    CHILD_PROCESS="$(pgrep -P "$SHELL_PID" java)"
-  done
-  echo "$CHILD_PROCESS" > "$PID_FILE"
-EOF
-
 JAVA_HOME="$1" nohup "$2" server -F -f "$3" > "$4" 2> "$5" < /dev/null &
-SHELL_PID="$!"
-
+echo "$!" > "$6"
 timeout 30 bash -c "$WAIT_FOR_LOG" "$4" || die "Timed out while waiting for $4"
-
-timeout 30 bash -c "$WAIT_FOR_CHILD" "$SHELL_PID" "$6" || die "Timed out while waiting for child java process to exist"
 
 #Want to wait until Jetty starts
 #See http://superuser.com/questions/270529/monitoring-a-file-until-a-string-is-found#answer-900134
