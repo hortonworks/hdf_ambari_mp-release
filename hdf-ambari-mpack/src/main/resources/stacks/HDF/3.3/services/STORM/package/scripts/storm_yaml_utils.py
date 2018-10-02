7de457405469efd39d556e610e7386f4fa1ddd8f
Sapin amin <samin@hortonworks.com>
2018-10-02 16:25:11 -0700

    BUG-111825 : Storm handling customized zookeeper principal server name

--- hdf-ambari-mpack/src/main/resources/stacks/HDF/3.3/services/STORM/package/scripts/storm_yaml_utils.py
+++ hdf-ambari-mpack/src/main/resources/stacks/HDF/3.3/services/STORM/package/scripts/storm_yaml_utils.py
@@ -25,12 +25,13 @@ from resource_management.core.source import InlineTemplate
 from resource_management.core.resources.system import File
 
 def replace_jaas_placeholder(name, security_enabled, conf_dir):
+  import params
   if name.find('_JAAS_PLACEHOLDER') > -1:
     if security_enabled:
       if name.find('Nimbus_JVM') > -1:
         return name.replace('_JAAS_PLACEHOLDER', '-Djava.security.auth.login.config=' + conf_dir + '/storm_jaas.conf -Djavax.security.auth.useSubjectCredsOnly=false')
       else:
-        return name.replace('_JAAS_PLACEHOLDER', '-Djava.security.auth.login.config=' + conf_dir + '/storm_jaas.conf')
+        return name.replace('_JAAS_PLACEHOLDER', '-Dzookeeper.sasl.client.username=' + params.zookeeper_principal_primary + ' -Djava.security.auth.login.config=' + conf_dir + '/storm_jaas.conf')
     else:
       return name.replace('_JAAS_PLACEHOLDER', '')
   else:
