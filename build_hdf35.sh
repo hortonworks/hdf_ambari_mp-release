32443319635144d2852d9706afa14f914ab133d0
Robert Kalmar <rkalmar@cloudera.com>
2020-07-13 11:35:27 +0200

    CFM-1354 HDF-3.5.2 mpack CFM-1105 Incorrect HDP-UTILS url in hdf-mpack CFM-1028 Identity strategy is duplicated in the login-identity-provider Ambari template

--- build_hdf35.sh
+++ build_hdf35.sh
@@ -1,4 +1,4 @@
 pushd hdf-ambari-mpack
-mvn versions:set -DnewVersion=3.5.1.0-1
+mvn versions:set -DnewVersion=3.5.2.0-1
 mvn clean package -DminAmbariVersion=2.7.0 -DmaxAmbariVersion=2.7.5 -Phorton
 popd
